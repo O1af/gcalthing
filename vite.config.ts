5a78a4ef6d57073161f672858dfd8f8af68dc996
olafpd <olafpd@umich.edu>
2026-03-30 17:12:13 -0400

    vite 8

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,25 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vite";
+import { devtools } from "@tanstack/devtools-vite";
 
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { cloudflare } from '@cloudflare/vite-plugin'
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
-const config = defineConfig({
+const config = defineConfig(({ mode }) => ({
+  resolve: {
+    tsconfigPaths: true,
+  },
   plugins: [
     devtools(),
-    cloudflare({ viteEnvironment: { name: 'ssr' } }),
-    tsconfigPaths({ projects: ['./tsconfig.json'] }),
+    ...(mode === "test"
+      ? []
+      : cloudflare({ viteEnvironment: { name: "ssr" } })),
     tailwindcss(),
     tanstackStart(),
     viteReact(),
   ],
-})
+}));
 
-export default config
+export default config;
